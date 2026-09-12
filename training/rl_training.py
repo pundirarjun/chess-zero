8f4026095d9216cc14835988356cc661c9b2dfec
pundir-arjun <pundirarjun400@gmail.com>
2026-09-12 20:43:03 +0530

    'Bug fix '

--- training/rl_training.py
+++ training/rl_training.py
@@ -33,8 +33,8 @@ from training.trainer import train_one_batch
 # ---------------------------------------------------------------------------
 RL_ITERATION = 7
 PREVIOUS_ITERATION = RL_ITERATION - 1
-KAGGLE_CHECKPOINT_ROOT = "/kaggle/working/chess-zero/checkpoints"
-LOCAL_CHECKPOINT_ROOT = "checkpoints"
+KAGGLE_CHECKPOINT_ROOT = "/kaggle/input/datasets/arjunthakur9999/checkpoints"
+LOCAL_CHECKPOINT_ROOT = "checkpoints"   
 PREVIOUS_CHECKPOINT = os.path.join(KAGGLE_CHECKPOINT_ROOT, f"rl_iteration_{PREVIOUS_ITERATION}.pt")
 PREVIOUS_REPLAY_BUFFER = os.path.join(KAGGLE_CHECKPOINT_ROOT, f"replay_buffer_rl{PREVIOUS_ITERATION}.pt")
 OUTPUT_CHECKPOINT = os.path.join(LOCAL_CHECKPOINT_ROOT, f"rl_iteration_{RL_ITERATION}.pt")
