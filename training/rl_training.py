60b445e5aafc5f9af05047c91b8cb299bb7b89eb
pundir-arjun <pundirarjun400@gmail.com>
2026-09-12 15:56:34 +0530

    ""

--- training/rl_training.py
+++ training/rl_training.py
@@ -33,7 +33,7 @@ from training.trainer import train_one_batch
 # ---------------------------------------------------------------------------
 RL_ITERATION = 6
 PREVIOUS_ITERATION = RL_ITERATION - 1
-KAGGLE_CHECKPOINT_ROOT = "/kaggle/working/chess-zero/checkpoints"
+KAGGLE_CHECKPOINT_ROOT = "/kaggle/input/datasets/arjunthakur9999/checkpoints"
 LOCAL_CHECKPOINT_ROOT = "checkpoints"
 PREVIOUS_CHECKPOINT = os.path.join(KAGGLE_CHECKPOINT_ROOT, f"rl_iteration_{PREVIOUS_ITERATION}.pt")
 PREVIOUS_REPLAY_BUFFER = os.path.join(KAGGLE_CHECKPOINT_ROOT, f"replay_buffer_rl{PREVIOUS_ITERATION}.pt")
