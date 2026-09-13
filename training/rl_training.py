77ed0ba0f0cfdcd8f698a2d8c76db383959d213b
pundir-arjun <pundirarjun400@gmail.com>
2026-09-13 13:33:25 +0530

    ''

--- training/rl_training.py
+++ training/rl_training.py
@@ -31,7 +31,7 @@ from training.trainer import train_one_batch
 # ---------------------------------------------------------------------------
 # Iteration / paths
 # ---------------------------------------------------------------------------
-RL_ITERATION = 9
+RL_ITERATION = 10
 PREVIOUS_ITERATION = RL_ITERATION - 1
 KAGGLE_CHECKPOINT_ROOT = "/kaggle/input/datasets/arjunthakur9999/checkpoints"
 LOCAL_CHECKPOINT_ROOT = "checkpoints"   
