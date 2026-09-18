0dc54dbe03bac2d8505e5c86b9c7a3a244a1cab2
pundir-arjun <pundirarjun400@gmail.com>
2026-09-18 17:16:52 +0530

    ''

--- evaluation/evaluate_models.py
+++ evaluation/evaluate_models.py
@@ -63,14 +63,14 @@ DEVICE = torch.device(
 # ==========================================================
 
 MODEL_A_CHECKPOINT = (
-    "/kaggle/input/datasets/arjunthakur9999/checkpoints/rl_iteration_11.pt"
+    "/kaggle/input/datasets/arjunthakur9999/checkpoints/rl_iteration_17.pt"
 )
 
 
 
 
 MODEL_B_CHECKPOINT = (
-    "/kaggle/working/chess-zero/checkpoints/rl_iteration_17.pt"
+    "/kaggle/working/chess-zero/checkpoints/rl_iteration_18.pt"
 )
 
 
