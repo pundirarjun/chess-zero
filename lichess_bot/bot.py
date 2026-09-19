f0f1674656f84c9563bdbaf5c3d101283ecdc7b2
pundir-arjun <pundirarjun400@gmail.com>
2026-09-19 10:23:39 +0530

    RL 3 vs maia3-79m_2000 2000 elo

--- lichess_bot/bot.py
+++ lichess_bot/bot.py
@@ -37,7 +37,7 @@ from lichess_client import LichessClient
 # SETTINGS
 # ============================================================
 
-OPPONENT = "WorstFish"
+OPPONENT = "maia3-79m_2000"
 
 NUM_SIMULATIONS = 100
 
